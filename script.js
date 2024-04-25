ec8d339a277240d6f74aee57535099e49aa19203
Rajiv kumar <rajivdhanama00@gmail.com>
2024-04-25 17:20:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container")
+
+function addTask(){
+    if(inputBox.value === ''){
+    alert("you must write something!");
+}else{
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";    ///u00d7 for cross icon
+    li.appendChild(span);
+    }
+    inputBox.value="";
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classlist.toggle("checked");
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+
+},flase);
